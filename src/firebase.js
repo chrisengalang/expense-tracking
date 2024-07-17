6b5d53a8ea058ab3e0592c7296807273bc303592
Chrisen Galang <chrisengalang@Chrisens-MacBook-Pro.local>
2024-07-16 18:04:26 -0700

    Initial commit of expense tracking app

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBY3GpLUKqgPkmo4ImtjZM7AjnbjouMriM",
+  authDomain: "expense-tracking-chrisen-dev.firebaseapp.com",
+  projectId: "expense-tracking-chrisen-dev",
+  storageBucket: "expense-tracking-chrisen-dev.appspot.com",
+  messagingSenderId: "820559788746",
+  appId: "1:820559788746:web:fe12fea3930d2a5accd233",
+  measurementId: "G-V2QZ8FYGR3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
